cea24a922a940d8b2386046d54cfdc29c115f1bc
Marley <mrl.menard@gmail.com>
2024-11-28 11:12:00 -0500

    Filled NaN values by the mean

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ print("Path to dataset files:", path)
 listDf = []
 for file in os.listdir(path):
     listDf.append(pd.read_csv(os.path.join(path, file)))
-df = pd.concat(listDf, ignore_index=True)
-print(df)
+dataset = pd.concat(listDf, ignore_index=True)
+print(dataset.head())
+
+dataset = dataset.fillna(dataset.mean())
+print(dataset.head())
